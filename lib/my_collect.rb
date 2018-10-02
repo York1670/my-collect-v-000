df0046ae954b79e9f6fbc7fe79871236318d9a60
Bobbi York <york1670@gmail.com>
2018-10-02 20:10:06 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ empty_collect = []
 def my_collect(collection)
   counter = 0
   while counter < collection.length
-    counter = counter +=1
+    counter +=1
     my_collect(collection) do |collect|
     empty_collect << yield collect.upcase
     empty_collect
